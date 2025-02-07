168dd2ed7d7009683a78b88b0518d5c25be15a9d
karennovaes <karen-nd@outlook.com>
2025-02-07 18:44:40 -0300

    Estilização da aplicação e ajustes no componente Logo

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -4,7 +4,7 @@ import './estilo.css';
 function Logo() {
     return (
         <div className="logo">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="logo-img" alt="logo" />
           <p> <strong>Alura</strong>Books</p>
         </div>
     );
