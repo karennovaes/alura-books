284d965c9cd1e9aa88b95a86e7f12885a31d0cff
karennovaes <karen-nd@outlook.com>
2025-02-07 18:24:31 -0300

    Começando com o primeiro componente: Logo!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import logo from './imagens/logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <div className="logo">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> <strong>Alura</strong>Books</p>
+        </div>
 
       </header>
     </div>
