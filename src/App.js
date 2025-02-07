20cf26fddbba5b5091bbbe6a5dda1ae5df79971b
karennovaes <karen-nd@outlook.com>
2025-02-07 19:43:43 -0300

    Usando Loops e refatorando o estilo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import Logo from './componentes/Logo';
+import perfil from './imagens/perfil.svg';
+import sacola from './imagens/sacola.svg';
+
+const  textOpcoes = ['CATEGORIAS', 'MINHA ESTANTE', 'FAVORITOS'];
+const icones = [perfil, sacola];
 
 function App() {
   return (
@@ -7,11 +12,24 @@ function App() {
       <header className="App-header">
         
         <Logo></Logo>
+        <ul className='opcoes'> 
+          {textOpcoes.map((texto) => ( 
+            <li className='opcao'><p>{texto}</p></li>
+          ))} 
+        </ul>
 
-
+        <ul className='icones'> 
+          {icones.map((icone) => ( 
+            <li className='icone'><img src={icone}></img></li>
+          ))} 
+                    
+        </ul>
       </header>
     </div>
   );
+  
 }
-
 export default App;
+ /* map é uma função que percorre um array e 
+ retorna um novo array com as modificações que 
+ você deseja  */
