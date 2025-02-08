9e033dd4d14ee91f29ff20be9431806c87dc627c
karennovaes <karen-nd@outlook.com>
2025-02-07 22:47:15 -0300

    Card reutilizável com props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,6 +1,8 @@
 import { livros } from './dadosUltimosLancamentos'
-import styled from 'styled-components'
 import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
+import styled from 'styled-components'
 
 
 const UltimosLancamentosContainer = styled.section`
@@ -31,6 +33,12 @@ function UltimosLancamentos() {
                     <img alt='livros' src={livro.src}/>
                 ) ) }
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
