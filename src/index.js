662bbb4274d6f0db2da550b9a450fe02140f3a28
karennovaes <karen-nd@outlook.com>
2025-02-10 20:41:52 -0300

    Refactor application structure: remove App component, add Home and Favoritos routes, and update Header for navigation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Home from "./rotas/Home";
 import reportWebVitals from "./reportWebVitals";
 import { createGlobalStyle } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./componentes/Header";
+import Favoritos from "./rotas/Favoritos";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -30,9 +32,10 @@ root.render(
   <React.StrictMode>
     <GlobalStyle />
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<App />  } />
-        <Route path="/favoritos" element={<p>Oi!</p>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/favoritos" element={<Favoritos />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
